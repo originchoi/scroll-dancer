51ba1534496a61cd23174db07dc61d26ce053e48
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-09-18 16:12:57 +0900

    Feat: integrate gsap with scrollControls to implement Timeline

--- src/components/Dancer.jsx
+++ src/components/Dancer.jsx
@@ -1,25 +1,76 @@
 import { useEffect, useRef } from "react";
 import { useAnimations, useGLTF, useScroll } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
+import gsap from "gsap";
 
 import { useRecoilValue } from "recoil";
 import { IsEnteredAtom } from "../stores";
 import { Loader } from "./Loader";
 
+let timeline;
+
 export const Dancer = () => {
+  const three = useThree();
   const isEntered = useRecoilValue(IsEnteredAtom);
   const dancerRef = useRef(null);
   const { scene, animations } = useGLTF("/models/dancer.glb");
   const { actions } = useAnimations(animations, dancerRef);
   const scroll = useScroll();
 
-  useFrame(() => {});
+  useFrame(() => {
+    if (!isEntered) return;
+
+    timeline.seek(scroll.offset * timeline.duration());
+  });
 
   useEffect(() => {
     if (!isEntered) return;
 
     actions["wave"].play();
-  }, [actions]);
+  }, [actions, isEntered]);
+
+  useEffect(() => {
+    if (!isEntered) return;
+    if (!dancerRef.current) return;
+
+    gsap.fromTo(
+      three.camera.position,
+      {
+        x: -5,
+        y: 5,
+        z: 5,
+      },
+      {
+        duration: 2.5,
+        x: 0,
+        y: 6,
+        z: 12,
+      }
+    );
+    gsap.fromTo(
+      three.camera.rotation,
+      { z: Math.PI },
+      {
+        duration: 2.5,
+        z: 0,
+      }
+    );
+  }, [isEntered, three.camera.position, three.camera.rotation]);
+
+  useEffect(() => {
+    if (!isEntered) return;
+    if (!dancerRef.current) return;
+
+    timeline = gsap.timeline();
+    timeline.from(
+      dancerRef.current.rotation,
+      {
+        duration: 4,
+        y: -4 * Math.PI,
+      },
+      0.5
+    );
+  }, [isEntered]);
 
   if (isEntered) {
     return (

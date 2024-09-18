f1d408c427f4423d12a350ec1c6c151df42b6346
ORIGINCHOI <116258834+ORIGINCHOI@users.noreply.github.com>
2024-09-18 17:06:04 +0900

    Feat: implement background and glitter effects

--- src/components/Dancer.jsx
+++ src/components/Dancer.jsx
@@ -1,6 +1,15 @@
-import { useEffect, useRef } from "react";
-import { useAnimations, useGLTF, useScroll } from "@react-three/drei";
+import { useEffect, useMemo, useRef } from "react";
+import {
+  Box,
+  Circle,
+  Points,
+  useAnimations,
+  useGLTF,
+  useScroll,
+  useTexture,
+} from "@react-three/drei";
 import { useFrame, useThree } from "@react-three/fiber";
+import * as THREE from "three";
 import gsap from "gsap";
 
 import { useRecoilValue } from "recoil";
@@ -14,7 +23,21 @@ export const Dancer = () => {
   const isEntered = useRecoilValue(IsEnteredAtom);
   const dancerRef = useRef(null);
   const { scene, animations } = useGLTF("/models/dancer.glb");
+  const texture = useTexture("/textures/5.png");
+
   const { actions } = useAnimations(animations, dancerRef);
+
+  const { positions } = useMemo(() => {
+    const count = 500;
+    const positions = new Float32Array(count * 3);
+
+    for (let i = 0; i < count * 3; i++) {
+      positions[i] = (Math.random() - 0.5) * 25;
+    }
+
+    return { positions };
+  }, []);
+
   const scroll = useScroll();
 
   useFrame(() => {
@@ -47,6 +70,7 @@ export const Dancer = () => {
         z: 12,
       }
     );
+
     gsap.fromTo(
       three.camera.rotation,
       { z: Math.PI },
@@ -62,21 +86,113 @@ export const Dancer = () => {
     if (!dancerRef.current) return;
 
     timeline = gsap.timeline();
-    timeline.from(
-      dancerRef.current.rotation,
-      {
-        duration: 4,
-        y: -4 * Math.PI,
-      },
-      0.5
-    );
-  }, [isEntered]);
+    timeline
+      .from(
+        dancerRef.current.rotation,
+        {
+          duration: 4,
+          y: -4 * Math.PI,
+        },
+        0.5
+      )
+      .from(
+        dancerRef.current.position,
+        {
+          duration: 4,
+          x: 3,
+        },
+        "<"
+      )
+      .to(
+        three.camera.position,
+        {
+          duration: 10,
+          x: 2,
+          z: 8,
+        },
+        "<"
+      )
+      .to(three.camera.position, {
+        duration: 10,
+        x: 0,
+        z: 6,
+      })
+      .to(three.camera.position, {
+        duration: 10,
+        x: 0,
+        z: 16,
+      });
+  }, [isEntered, three.camera.position]);
 
   if (isEntered) {
     return (
       <>
-        <ambientLight intensity={2} />
         <primitive ref={dancerRef} object={scene} scale={0.05} />;
+        <ambientLight intensity={2} />
+        <rectAreaLight position={[0, 10, 0]} intensity={30} />
+        <pointLight
+          position={[0, 5, 0]}
+          intensity={45}
+          castShadow
+          receiveShadow
+        />
+        <hemisphereLight
+          position={[0, 5, 0]}
+          intensity={0}
+          groundColor={"lime"}
+          color="blue"
+        />
+        0
+        <Box position={[0, 0, 0]} args={[100, 100, 100]}>
+          <meshStandardMaterial color={"#DC4F00"} side={THREE.DoubleSide} />
+        </Box>
+        <Circle
+          castShadow
+          receiveShadow
+          args={[8, 32]}
+          rotation-x={-Math.PI / 2}
+          position-y={-4.4}
+        >
+          <meshStandardMaterial color={"#DC4F00"} side={THREE.DoubleSide} />
+        </Circle>
+        <Points positions={positions.slice(0, positions.length / 3)}>
+          <pointsMaterial
+            size={0.5}
+            color={new THREE.Color("#DC4F00")}
+            sizeAttenuation
+            depthWrite
+            alphaMap={texture}
+            transparent
+            alphaTest={0.001}
+          />
+        </Points>
+        <Points
+          positions={positions.slice(
+            positions.length / 3,
+            (positions.length * 2) / 3
+          )}
+        >
+          <pointsMaterial
+            size={0.5}
+            color={new THREE.Color("#DC4F00")}
+            sizeAttenuation
+            depthWrite
+            alphaMap={texture}
+            transparent
+            alphaTest={0.001}
+          />
+        </Points>
+        <Points positions={positions.slice((positions.length * 2) / 3)}>
+          <pointsMaterial
+            size={0.5}
+            color={new THREE.Color("#DC4F00")}
+            sizeAttenuation
+            depthWrite
+            alphaMap={texture}
+            transparent
+            alphaTest={0.001}
+          />
+        </Points>
       </>
     );
   }
